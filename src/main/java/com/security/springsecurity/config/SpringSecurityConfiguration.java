5e52fc1e0344ef3887a33f93f9527c3cebf5209c
BorisDvn <dovonon@th-brandenburg.de>
2021-12-15 15:53:37 +0100

    ok: getToken

--- src/main/java/com/security/springsecurity/config/SpringSecurityConfiguration.java
+++ src/main/java/com/security/springsecurity/config/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.security.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +31,12 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // without CSRF
@@ -37,6 +44,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/helloadmin").hasRole("ADMIN")
                 .antMatchers("/hellouser").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/authenticate").permitAll().anyRequest().authenticated()
                 .and().httpBasic();
     }
 
